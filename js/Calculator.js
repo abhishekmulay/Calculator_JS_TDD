caf9fb6516893c3c075b998358de5132b661f7a0
abhishekmulay <mulaya@pun-mulaya-m.ds.ad.adp.com>
2014-08-01 10:16:33 +0530

    Added basic methods of each type
    Added methods for testing DOM manipulation

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,15 +1,28 @@
 (function(){
-	function Controller(){
 
+	function Controller(context){
+		this.context = $(context);
 	};
 
-	Controller.prototype.init = function(){
-
+	Controller.prototype.add = function(num1 , num2 ){
+		return num1+num2;
 	};
 
-	Controller.prototype.bindEvents = function(){
-		
+	Controller.prototype.clear = function(){
+		console.log('in clear');
 	};
+
+    Controller.prototype.bindEvents = function(){
+        var self = this;
+
+        this.context.find('#clear-btn').click(function(){
+            self.clear();
+        });
+    };
+
+    Controller.prototype.init = function(){
+        this.bindEvents();
+    };
 
 	window.CalculatorController = Controller;
 })();
--- spec/Calculator.spec.js
+++ spec/Calculator.spec.js
@@ -1,17 +1,22 @@
-// we will start with writting a test suite using jasmine
-
 describe("Calculator test suite",function(){
+
+	var controller ,context ;
+	beforeEach(function(){
+        context = jQuery('<div />').attr('id','calculator_widget');
+		controller  = new CalculatorController(context);
+		controller.init();
+	});
 
 	describe("basic setup for the calculator testing",function(){
 
 		it("shuould expose the calculator controller",function(){
-			var controller  = new CalculatorController();
+		
 			expect(controller).toBeDefined();
 			expect(typeof CalculatorController).toBe('function');
 		});
 
-		describe("should have methods on prototype ",function(){
-			var methods = ['init','bindEvents'];
+		describe("Checking methods on prototype ",function(){
+			var methods = ['init','bindEvents','add','clear'];
 			exposesMethods(CalculatorController.prototype,methods);
 		});
 
@@ -24,7 +29,28 @@ describe("Calculator test suite",function(){
 			});
 		}
 
-
 	});
+
+	describe("basic arithmatic operations test suite",function(){
+		var num1, num2;
+		it("should add two numbers",function(){
+			num1 = 20 , num2 = 50;
+			expect(controller.add(num1,num2)).toEqual(num1+num2);
+		});	
+	});
+
+	describe("Button functionality test suite",function(){
+
+		it('should call clear() when clear button is clicked',function(){
+		 	var clearButtonStub = $('<button />').attr('id','clear-btn');
+		 	clearButtonStub.appendTo(context);
+            controller.bindEvents();
+            spyOn(controller,"clear");
+
+            clearButtonStub.trigger('click');
+
+		 	expect(controller.clear).toHaveBeenCalled();	
+		});
+	 });
 
 });
